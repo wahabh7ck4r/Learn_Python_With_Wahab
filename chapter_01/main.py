1060efd5424098416b1da8eb65c2a9d795340df5
Abdul Wahab <abdulwahabchudhary@gmail.com>
2024-08-30 11:09:51 -0700

    first commit

---
+++ chapter_01/main.py
@@ -0,0 +1,18 @@
+import datetime
+
+# this line  hello world on screen 
+print("Hello world")
+
+# yah haamd gndo ke lea comment hai 
+print("hammad gndo hai ")
+
+print(datetime.time())
+
+# type of comment 
+# 1. signle line comment  (#)
+# 2. multi line comment  (''' command ''') 
+
+
+# two type of module 
+# 1. built in module eg. datetime
+# 2. user define module eg. chapter_1 
---
+++ chapter_01/user_define_module_example_01.py
@@ -0,0 +1,4 @@
+import chapter_1
+
+# example of using user define module 
+print(chapter_1)
