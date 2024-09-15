766783726f81d3b8ff836b9866118655c8098223
wahabh7ck4r <abdulwahabchudhary@gmail.com>
2024-09-15 14:35:37 +0500

    adding files

---
+++ Chapter_03/05_escape_sequence.py
@@ -0,0 +1,5 @@
+# using escap sequence to edit text - how can think escap sequence as a tool that you can use to edit your text just like you use photo editor to edit your photos
+text = 'we\'re going to cover some new topic:\n\thow to think big\n\thow to manage work\\activities'
+
+# print the edit text
+print(text)
