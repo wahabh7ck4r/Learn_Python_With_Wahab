b5fb2224dcf75a00b0cc398c0edd8ae9cbfe5879
wahabh7ck4r <abdulwahabchudhary@gmail.com>
2024-09-28 22:58:07 +0500

    adding file

---
+++ Chapter_06/01_ifElse.py
@@ -0,0 +1,16 @@
+# taking name from user 
+name = input("Enter you name: ").lower() # to keep in lowerCase.
+
+# if name is wahab than return "you are great" other wise skip the whatever inside it.
+if name == "wahab":
+    print("you are great")  # if the condition true than this will excute otherwise 
+
+
+# if with else 
+age = int(input('Enter you age : ')) # take age from user 
+
+# if user is grater than 15 than print inside of if excute other else excute 
+if age > 15: 
+    print('you can marry') # if condition is true 
+else:
+    print("you are kid, keep waitin...")    #if condition is false  
